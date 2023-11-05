485ee331d8b06f8e8beee6a7edd4bb9c5271dd54
Ефремов <efremov.ad@edu.spbstu.ru>
2023-11-05 16:28:28 +0300

    start

---
+++ pkg/main.go
@@ -0,0 +1,28 @@
+package pkg
+
+import "os"
+
+func Contains(way string, searchIt string) (bool, error) {
+	content, err := os.ReadFile(way)
+	if err != nil {
+		return false, err
+	}
+	return searcher(string(content), searchIt), err
+}
+
+func searcher(content string, searchIt string) bool {
+	result := false
+	for i := 0; i < len(content); i++ {
+		if content[i] == searchIt[0] && len(searchIt) >= len(content) {
+			k := i + 1
+			for j := 2; j < len(searchIt); j++ {
+				if content[k] != searchIt[j] {
+					break
+				}
+				k++
+			}
+			result = true
+		}
+	}
+	return result
+}
