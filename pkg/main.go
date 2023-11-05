147ca4ba4113e15699bb5355f94b04e060e1f276
Ефремов <efremov.ad@edu.spbstu.ru>
2023-11-05 17:59:38 +0300

    v5

--- pkg/main.go
+++ pkg/main.go
@@ -1,11 +1,17 @@
 package pkg
 
-import "os"
+import (
+	"errors"
+	"os"
+)
 
 func Contains(way string, searchIt string) (bool, error) {
 	content, err := os.ReadFile(way)
 	if err != nil {
-		return false, err
+		return false, errors.New("problems with the path")
+	}
+	if len(searchIt) < 1 {
+		return false, errors.New("empty character")
 	}
 	return searcher(string(content), searchIt), err
 }
@@ -14,8 +20,9 @@ func searcher(content string, searchIt string) bool {
 	resultAssist := false
 	for i := 0; i < len(content); i++ {
 		if content[i] == searchIt[0] && len(searchIt) <= len(content) {
+			resultAssist = true
 			k := i + 1
-			for j := 2; j < len(searchIt); j++ {
+			for j := 1; j < len(searchIt); j++ {
 				if content[k] != searchIt[j] {
 					resultAssist = false
 					break
